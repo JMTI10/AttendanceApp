033673998884fbf812aa985fced88e241cd3a5f9
JMTI10 <idg@live.com.pt>
2025-06-24 17:48:38 +0300

    Changes to be committed: 	modified:   app/(tabs)/index.tsx 	new file:   firebaseConfig.js 	modified:   package-lock.json 	modified:   package.json

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-uBnjuP5CcKCLYrFkL3eo3V-ngeFZVzo",
+  authDomain: "attendace-checker-101.firebaseapp.com",
+  databaseURL: "https://attendace-checker-101-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "attendace-checker-101",
+  storageBucket: "attendace-checker-101.firebasestorage.app",
+  messagingSenderId: "948184339796",
+  appId: "1:948184339796:web:482ca83a7a5983f423a4c1",
+  measurementId: "G-SM7ELXG5NL"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
