ece3e2977f1eae8f3076542ca9c6054763433278
JMTI10 <idg@live.com.pt>
2025-06-24 19:19:00 +0300

    Changes to be committed: 	modified:   app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
 import { Stack, useRouter } from 'expo-router';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { getAuthInstance } from '../firebaseConfig';
+import { Text, View } from 'react-native';
+import { getAuthInstance } from '../firebaseConfig'; // or adjust path as needed
 
 export default function RootLayout() {
   const router = useRouter();
   const [checkingAuth, setCheckingAuth] = useState(true);
-  const auth = getAuthInstance();
-  
 
   useEffect(() => {
+    const auth = getAuthInstance();
+
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         router.replace('/(tabs)');
@@ -22,7 +23,13 @@ export default function RootLayout() {
     return () => unsubscribe();
   }, []);
 
-  if (checkingAuth) return null;
+  if (checkingAuth) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0C0F14' }}>
+        <Text style={{ color: '#fff' }}>Loading...</Text>
+      </View>
+    );
+  }
 
   return <Stack screenOptions={{ headerShown: false }} />;
 }
